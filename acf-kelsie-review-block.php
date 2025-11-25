2eee114dbefcefa1855aadadf16bd07dd6d14958
kelsiecakessugarflowers <kelsie@kelsiecakes.com>
2025-11-25 17:47:18 -0500

    Fix render template structure and bump version

--- acf-kelsie-review-block.php
+++ acf-kelsie-review-block.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Kelsie ACF Reviews Block
  * Description: ACF block for displaying Reviews repeater content with optional Rank Math schema integration.
- * Version:     1.1.5
+ * Version:     1.1.6
  * Author:      Kelsie Cakes
  */
 
